b5251aaabf392f6772b69b2cd22972bde0579da9
JoaoVictorSicco <victordesicco@gmail.com>
2024-03-29 09:35:15 -0400

    fix: imprimir mensagem de log de cadastro

--- c1_python_envio_uart/envio_uart_python.py
+++ c1_python_envio_uart/envio_uart_python.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # Defina a variável global
-mensagem_serial = 'Teste'
+mensagem_serial = 'Bom dia'
 
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)  # ajuste a porta conforme necessário
 intervalo = 3
@@ -10,10 +10,10 @@ intervalo = 3
 for i in range(intervalo, 0, -1):
     print(f"cadastrando em {i}")
     time.sleep(1)
-print("ID Cadastrado")
 
 # Use a variável global no lugar de 'Hello World'
 ser.write(mensagem_serial.encode('utf-8'))
+print("ID Cadastrado")
 time.sleep(1)
 
 ser.close()
